90340f8c17f38febe9868cc29da291c1710f0029
Joe Lewis <joelewis101@doctors.org.uk>
2026-08-18 12:18:07 +0100

    Add all sample types to cleaning script

--- scripts/clean_micro_data.R
+++ scripts/clean_micro_data.R
@@ -2,11 +2,31 @@ library(tidyverse)
 library(here)
 library(tidyxl)
 
-file_path <- here("data/raw/Sample Result (Spreadsheet-Excel)/Spreadsheet Sampel INPATIENT.xlsx")
+# Strategy:
+# Identify tables in the spreadsheets by contiguous cells with borders
+# Restrict to those that have "subjectid" in at least one cell (removes the
+# summary tables)
+# These tables have hierarchical merged headers eg
+#  ---------------------------------------------
+#  |       header 1                            |
+#  ---------------------------------------------
+#  | header 2.1       |  header 2.2            |
+#  ---------------------------------------------
+#  | hd 3.1 | hd 3.2  | hd 3.3   |  hd 3.4     |
+#  ---------------------------------------------
+#
+# to deal with these, sequentially merge headers so the first col will
+# be (for example) header1_header2.1_hd3.1
+# remove spaces and add underscore between
+#
+# the function extract_bordered_tables will do all of this
+#
+#
+# There are then sample type specific cleaning functions
 
-
-extract_bordered_tables <- function(file_path, sheet_name = 1) {
-  # 1. Read all cells and all formats
+extract_bordered_tables <- function(file_path, sheet_name = 1, n_header_rows = 2) {
+ 
+# 1. Read all cells and all formats
   all_cells <- xlsx_cells(file_path, sheets = sheet_name)
   all_formats <- xlsx_formats(file_path)
 
@@ -71,33 +91,39 @@ extract_bordered_tables <- function(file_path, sheet_name = 1) {
 
   # 5. mung headers to account for merged cells
 
-  mung_headers <- function(sub_table) {
-    header1 <- filter(sub_table, row == min(row))
-    header2 <- filter(sub_table, row == min(row) + 1)
-    header_final <-
-      left_join(header2, header1 |> ungroup() |> select(-row), by = "col") |>
-      arrange(col) |>
-      fill(value.y) |>
-      mutate(value.x = if_else(is.na(value.x), value.y, value.x)) |>
-      mutate(
-        value =
-          if_else(value.x != value.y,
-            paste0(
-              tolower(gsub(" ", "", value.y)),
-              "_", tolower(value.x)
-            ),
-            tolower(gsub(" ", "_", value.x))
-          )
-      ) |>
-      select(-c(value.x, value.y))
-    sub_table <-
-      bind_rows(
-        header_final,
-        sub_table |>
-          filter(row > min(row) + 1)
-      )
+  mung_hierarchical_headers <- function(sub_table, n_header_rows = 2) {
+    if (n_header_rows < 2) {
+      stop("Problem in mung_hierarchical_headers: n_header rows must be at least 2")
+    }
+
+    for (i in 1:(n_header_rows - 1)) {
+      header1 <- filter(sub_table, row == min(row))
+      header2 <- filter(sub_table, row == min(row) + 1)
+
+      header_final <-
+        left_join(header2, header1 |> ungroup() |> select(-row), by = "col") |>
+        arrange(col) |>
+        fill(value.x, value.y) |>
+        mutate(
+          value =
+            if_else(is.na(value.x),
+              paste0(tolower(gsub(" ", "", value.y))),
+              paste0(tolower(gsub(" ", "", value.y)), "_", tolower(gsub(" ", "", value.x)))
+            )
+        ) |>
+        select(-c(value.x, value.y))
+
+      sub_table <-
+        bind_rows(
+          header_final,
+          sub_table |>
+            filter(row > min(row) + 1)
+        )
+    }
     return(sub_table)
   }
+
+
   # and 6 Reshape each isolated table cluster back into a rectangular tibble
   tables_list <- clustered_cells %>%
   group_split(table_id) %>%
@@ -113,7 +139,7 @@ extract_bordered_tables <- function(file_path, sheet_name = 1) {
         select(row, col, value)
 
       if ("Subject ID" %in% sub_table$value) {
-        sub_table <- mung_headers(sub_table)
+        sub_table <- mung_hierarchical_headers(sub_table, n_header_rows)
       }
 
       # Pivot wide to re-create the Excel spreadsheet shape
@@ -136,20 +162,22 @@ extract_bordered_tables <- function(file_path, sheet_name = 1) {
   return(tables_list)
 }
 
+# inpatients
 
-# extract_bordered_tables(file_path, 2) 
+
+file_path <- here("data/raw/Sample Result (Spreadsheet-Excel)/Spreadsheet Sampel INPATIENT.xlsx")
 
 make_inpatient_data_tidy <- function(table_list) {
 
-table_list <- table_list[map(table_list, function(x) "subject_id" %in% colnames(x)) |> unlist()]
+table_list <- table_list[map(table_list, function(x) "subjectid" %in% colnames(x)) |> unlist()]
 
-map(table_list, \(x) fill(x, c(no, subject_id))) |>
+map(table_list, \(x) fill(x, c(no, subjectid))) |>
   bind_rows() |> 
-  pivot_longer(-c(no, subject_id),
+  pivot_longer(-c(no, subjectid),
   names_to = c("day", "plate"),
   names_pattern = "swab(.*)_(.*)") |>
   filter(!is.na(value), value != "-", value != "G") |>
-  group_by(no,subject_id, day, plate) |>
+  group_by(no,subjectid, day, plate) |>
   mutate(growth = if_else(all(value == "NG"), FALSE, TRUE),
          species  = paste(value, collapse = ";")) |>
   mutate(species = if_else(species == "NG", NA, species)) |> 
@@ -164,20 +192,21 @@ map(table_list, \(x) fill(x, c(no, subject_id))) |>
 df <-
   bind_rows(
     make_inpatient_data_tidy(
-      extract_bordered_tables(file_path, 1)
+      extract_bordered_tables(file_path, sheet_name = 1, n_header_rows = 2)
     ),
     make_inpatient_data_tidy(
-      extract_bordered_tables(file_path, 2)
+      extract_bordered_tables(file_path, sheet_name = 2, n_header_rows = 2)
     ),
     make_inpatient_data_tidy(
-      extract_bordered_tables(file_path, 3)
+      extract_bordered_tables(file_path, sheet_name = 3, n_header_rows = 2)
     )
   ) 
 
 
+
 df <-
   df |>
-  separate_wider_delim(subject_id, delim = ".", names = c("sid_1", "hospital", "ward", "pid")) |>
+  separate_wider_delim(subjectid, delim = ".", names = c("sid_1", "hospital", "sample_type", "pid")) |>
   mutate(
     hospital =
       case_when(
@@ -185,11 +214,174 @@ df <-
         hospital == 2 ~ "RSWN",
         hospital == 3 ~ "RSDA"
       ),
-    ward = case_when(
-      ward == 1 ~ "ward",
-      ward == 2 ~ "ICU"
+    sample_type = case_when(
+      sample_type == 1 ~ "ward",
+      sample_type  == 2 ~ "ICU",
+      sample_type == 3 ~ "community",
+      sample_type  == 5 ~ "staff",
+      sample_type  == 4 ~ "environment"
     )
   )
 
 write_csv(df, here("data/processed/cleaned_inpatient_cultures.csv"))
+
+
+# staff
+
+
+file_path <- here("data/raw/Sample Result (Spreadsheet-Excel)/Spreadsheet Sampel HEALTHWORKERS.xlsx")
+
+#extract_bordered_tables(file_path, sheet_name = 2, n_header_rows = 3) -> table_list
+
+make_staff_data_tidy <- function(table_list) {
+
+table_list <- table_list[map(table_list, function(x) "subjectid" %in% colnames(x)) |> unlist()]
+
+map(table_list, \(x) fill(x, c(no, subjectid))) |>
+  bind_rows() |> 
+  pivot_longer(-c(no, subjectid),
+  names_to = c("day", "sample", "plate"),
+  names_sep = "_") |>
+  filter(!is.na(value), value != "-", value != "G") |>
+  group_by(no,subjectid, day, sample, plate) |>
+  mutate(growth = if_else(all(value == "NG"), FALSE, TRUE),
+         species  = paste(value, collapse = ";")) |>
+  mutate(species = if_else(species == "NG", NA, species)) |> 
+  select(-value) |>
+  unique() |>
+  as.data.frame()
+
+}
+
+
+df <- bind_rows( make_staff_data_tidy(
+      extract_bordered_tables(file_path, sheet_name = 1, n_header_rows = 3)
+    ),
+    make_staff_data_tidy(
+      extract_bordered_tables(file_path, sheet_name = 2, n_header_rows = 3)
+    ),
+    make_staff_data_tidy(
+      extract_bordered_tables(file_path, sheet_name = 3, n_header_rows = 3)
+    )
+  ) 
+
+
+df <-
+  df |>
+  separate_wider_delim(subjectid, delim = ".", names = c("sid_1", "hospital", "sample_type", "pid")) |>
+  mutate(
+    hospital =
+      case_when(
+        hospital == 1 ~ "RSDK",
+        hospital == 2 ~ "RSWN",
+        hospital == 3 ~ "RSDA"
+      ),
+    sample_type = case_when(
+      sample_type == 1 ~ "ward",
+      sample_type  == 2 ~ "ICU",
+      sample_type  == 5 ~ "staff"
+
+    )
+  )
+
+write_csv(df, here("data/processed/cleaned_staff_cultures.csv"))
+
+### environment
+
+file_path <- here("data/raw/Sample Result (Spreadsheet-Excel)/Spreadsheet Sampel ENVIRONMENT.xlsx")
+
+make_env_data_tidy <- function(table_list) {
+
+table_list <- table_list[map(table_list, function(x) "subjectid" %in% colnames(x)) |> unlist()]
+
+map(table_list, \(x) fill(x, c(no, subjectid))) |>
+  bind_rows() |> 
+  rename(cro = result_cro, esbl = result_esbl) |>
+  pivot_longer(-c(no, subjectid),
+  names_to = c("plate")) |>
+  filter(!is.na(value), value != "-", value != "G") |>
+  group_by(no,subjectid, plate) |>
+  mutate(growth = if_else(all(value == "NG"), FALSE, TRUE),
+         species  = paste(value, collapse = ";")) |>
+  mutate(species = if_else(species == "NG", NA, species)) |> 
+  select(-value) |>
+  unique() |>
+  as.data.frame()
+
+}
+
+
+df <- bind_rows( 
+    make_env_data_tidy(
+      extract_bordered_tables(file_path, sheet_name = 1, n_header_rows = 2)
+    ),
+    make_env_data_tidy(
+      extract_bordered_tables(file_path, sheet_name = 2, n_header_rows = 2)
+    ),
+    make_env_data_tidy(
+      extract_bordered_tables(file_path, sheet_name = 3, n_header_rows = 2)
+    )
+  ) 
+
+
+df <-
+  df |>
+  separate_wider_delim(subjectid, delim = ".", names = c("sid_1", "hospital", "sample_type", "pid")) |>
+  mutate(
+    hospital =
+      case_when(
+        hospital == 1 ~ "RSDK",
+        hospital == 2 ~ "RSWN",
+        hospital == 3 ~ "RSDA"
+      ),
+    sample_type = case_when(
+      sample_type == 1 ~ "ward",
+      sample_type  == 2 ~ "ICU",
+      sample_type  == 5 ~ "staff",
+      sample_type  == 4 ~ "environment"
+    )
+  )
+
+write_csv(df, here("data/processed/cleaned_env_cultures.csv"))
+
+## COMMUNITY
+
+
+file_path <- here("data/raw/Sample Result (Spreadsheet-Excel)/Spreadsheet Sampel COMMUNITY.xlsx")
+
+# env tidy function also works here
+
+df <- bind_rows( 
+    make_env_data_tidy(
+      extract_bordered_tables(file_path, sheet_name = 1, n_header_rows = 2)
+    ),
+    make_env_data_tidy(
+      extract_bordered_tables(file_path, sheet_name = 2, n_header_rows = 2)
+    ),
+    make_env_data_tidy(
+      extract_bordered_tables(file_path, sheet_name = 3, n_header_rows = 2)
+    )
+  ) 
+
+
+df <-
+  df |>
+  separate_wider_delim(subjectid, delim = ".", names = c("sid_1", "hospital", "sample_type", "pid")) |>
+  mutate(
+    hospital =
+      case_when(
+        hospital == 1 ~ "RSDK",
+        hospital == 2 ~ "RSWN",
+        hospital == 3 ~ "RSDA"
+      ),
+    sample_type = case_when(
+      sample_type == 1 ~ "ward",
+      sample_type  == 2 ~ "ICU",
+      sample_type == 3 ~ "community",
+      sample_type  == 5 ~ "staff",
+      sample_type  == 4 ~ "environment"
+    )
+  )
+
+write_csv(df, here("data/processed/cleaned_community_cultures.csv"))
 
